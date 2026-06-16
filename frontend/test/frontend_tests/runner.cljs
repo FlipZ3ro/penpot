ca7af3f197b9cd510f2c7d557e7d28e46ba95188
alonso.torres <alonso.torres@kaleidos.net>
2026-06-16 15:16:38 +0200

    :bug: Fix problem with plugins creating interactions always added a new flow

--- frontend/test/frontend_tests/runner.cljs
+++ frontend/test/frontend_tests/runner.cljs
@@ -11,6 +11,7 @@
    [frontend-tests.data.uploads-test]
    [frontend-tests.data.viewer-test]
    [frontend-tests.data.workspace-colors-test]
+   [frontend-tests.data.workspace-interactions-test]
    [frontend-tests.data.workspace-mcp-test]
    [frontend-tests.data.workspace-media-test]
    [frontend-tests.data.workspace-shortcuts-test]
@@ -26,6 +27,7 @@
    [frontend-tests.logic.pasting-in-containers-test]
    [frontend-tests.main-errors-test]
    [frontend-tests.plugins.context-shapes-test]
+   [frontend-tests.plugins.interactions-test]
    [frontend-tests.plugins.page-test]
    [frontend-tests.plugins.parser-test]
    [frontend-tests.plugins.tokens-test]
@@ -64,6 +66,7 @@
     frontend-tests.data.uploads-test
     frontend-tests.data.viewer-test
     frontend-tests.data.workspace-colors-test
+    frontend-tests.data.workspace-interactions-test
     frontend-tests.data.workspace-mcp-test
     frontend-tests.data.workspace-media-test
     frontend-tests.data.workspace-shortcuts-test
@@ -77,6 +80,7 @@
     frontend-tests.logic.groups-test
     frontend-tests.logic.pasting-in-containers-test
     frontend-tests.plugins.context-shapes-test
+    frontend-tests.plugins.interactions-test
     frontend-tests.plugins.page-test
     frontend-tests.plugins.parser-test
     frontend-tests.plugins.tokens-test
